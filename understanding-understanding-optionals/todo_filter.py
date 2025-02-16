7ae4216c3e0ebadc502a30a2307ab9497e7c30c1
Anish Tondwalkar <anish.tondwalkar@gmail.com>
2025-02-16 08:11:33 -0800

    TODOs should not introduce paragraph breaks

--- understanding-understanding-optionals/todo_filter.py
+++ understanding-understanding-optionals/todo_filter.py
@@ -5,13 +5,13 @@ def latex_todo_to_html(elem, doc):
     if isinstance(elem, pf.RawInline) and elem.format == 'tex':
         if elem.text.startswith(r"\todo{") and elem.text.endswith("}"):
             content = elem.text[6:-1]  # Extract text inside \todo{}
-            return pf.RawInline(f"<aside>{content}</aside>", format="html")
+            return pf.RawInline(f'<span class=aside>{content}</span>', format='html')
         if elem.text.startswith(r"\AT{") and elem.text.endswith("}"):
             content = elem.text[4:-1]
-            return pf.RawInline(f"<aside class=AT>AT: {content}</aside>", format="html")
+            return pf.RawInline(f'<span class="aside AT">AT: {content}</span>', format='html')
         if elem.text.startswith(r"\AS{") and elem.text.endswith("}"):
             content = elem.text[4:-1]
-            return pf.RawInline(f"<aside class=AS>AS: {content}</aside>", format="html")
+            return pf.RawInline(f'<span class="aside AS">AS: {content}</span>', format='html')
 
 def main():
     pf.run_filter(latex_todo_to_html)

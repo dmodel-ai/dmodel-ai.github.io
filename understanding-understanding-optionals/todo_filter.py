9fb3129d9196fcac7c07f7c7f79793db840606d1
Anish Tondwalkar <anish.tondwalkar@gmail.com>
2025-02-24 12:15:08 -0800

    fix todo filter

--- understanding-understanding-optionals/todo_filter.py
+++ understanding-understanding-optionals/todo_filter.py
@@ -13,6 +13,17 @@ def latex_todo_to_html(elem, doc):
             content = elem.text[4:-1]
             return pf.RawInline(f'<span class="aside AS">AS: {content}</span>', format='html')
 
+    if isinstance(elem, pf.RawBlock) and elem.format == 'tex':
+        if elem.text.startswith(r"\todo{") and elem.text.endswith("}"):
+            content = elem.text[6:-1]  # Extract text inside \todo{}
+            return pf.RawBlock(f'<span class=aside>{content}</span>', format='html')
+        if elem.text.startswith(r"\AT{") and elem.text.endswith("}"):
+            content = elem.text[4:-1]
+            return pf.RawBlock(f'<span class="aside AT">AT: {content}</span>', format='html')
+        if elem.text.startswith(r"\AS{") and elem.text.endswith("}"):
+            content = elem.text[4:-1]
+            return pf.RawBlock(f'<span class="aside AS">AS: {content}</span>', format='html')
+
 def main():
     pf.run_filter(latex_todo_to_html)
 

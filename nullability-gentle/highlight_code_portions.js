aefedeba18560e93a472b0e5afe8c88beadbce40
Alex Sanchez-Stern <alex.sanchezstern@gmail.com>
2025-03-19 14:37:10 -0700

    Oops remove some debug stuff

--- nullability-gentle/highlight_code_portions.js
+++ nullability-gentle/highlight_code_portions.js
@@ -4,16 +4,12 @@
         code_blocks = Array.from(document.querySelectorAll("code.sourceCode"));
         console.log("There are " + code_blocks.length + " code blocks");
         code_blocks.forEach((ref, index) => {
-            if (index != 4) {
-                return;
-            }
             // For each line
             for (const line of ref.children) {
                 let in_block = false;
                 let cur_contents = [];
                 let newChildren = []
                 for (const child of line.childNodes) {
-                    console.log(child)
                     if (child.nodeName == "SPAN" && child.className == "op" && child.textContent == "==") {
                         if (in_block) {
                             let newMark = document.createElement("mark");
